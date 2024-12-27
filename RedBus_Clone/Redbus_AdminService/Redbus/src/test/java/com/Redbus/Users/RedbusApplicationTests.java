54e0098f6da57d0a91fa55c6de784e87db6db94f
shashank0711-sm <shashank.shrivastava0711@gmail.com>
2024-12-27 21:40:56 +0530

    Add files via upload

---
+++ RedBus_Clone/Redbus_AdminService/Redbus/src/test/java/com/Redbus/Users/RedbusApplicationTests.java
@@ -0,0 +1,13 @@
+package com.Redbus.Users;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RedbusApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
---
+++ RedBus_Clone/Redbus_PaymentService/Redbus/src/test/java/com/Redbus/Payment/RedbusApplicationTests.java
@@ -0,0 +1,13 @@
+package com.Redbus.Payment;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RedbusApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
